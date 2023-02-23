6fd03b2dc8385563cf9467f20f10f2f25a5fac12
ani-sha22 <anisha.chakraborty22@gmail.com>
2023-02-23 17:19:38 +0530

    dekh

--- input-output.py
+++ input-output.py
@@ -6,6 +6,7 @@ print("Hello User!\nWelcome to simple calculator\n")
 num1 = int(input("Enter number 1 : "))
 op = input("Enter operation as symbol : ")
 num2 = int(input("Enter number 2 : "))
+print("yolo")
 
 if(op == '+'): print("Output = ",addition(num1,num2));
 if(op == '-'): print("Output = ",sub(num1,num2));
