abc0348ee5bfbf700bed1ec220f919aef1f5f46c
Nehel Khanna <nehel.khanna20@st.niituniversity.in>
2023-02-23 17:21:32 +0530

    dekhlo baby ahunnnn

--- input-output.py
+++ input-output.py
@@ -7,6 +7,7 @@ num1 = int(input("Enter number 1 : "))
 op = input("Enter operation as symbol : ")
 num2 = int(input("Enter number 2 : "))
 print("yolo")
+print("hello kya haal chaal soniye")
 
 if(op == '+'): print("Output = ",addition(num1,num2));
 if(op == '-'): print("Output = ",sub(num1,num2));
