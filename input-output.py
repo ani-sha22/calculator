955db3efb3033e661ff1204496b00f1e4898f819
Nehel Khanna <nehel.khanna20@st.niituniversity.in>
2023-02-23 17:13:47 +0530

    CALCULATORRRRRRRRRRR ALIGATORRRRRRRR

---
+++ input-output.py
@@ -0,0 +1,12 @@
+from addition import addition
+from multiplication import multiply
+from subtract import sub
+
+print("Hello User!\nWelcome to simple calculator\n")
+num1 = int(input("Enter number 1 : "))
+op = input("Enter operation as symbol : ")
+num2 = int(input("Enter number 2 : "))
+
+if(op == '+'): print("Output = ",addition(num1,num2));
+if(op == '-'): print("Output = ",sub(num1,num2));
+if(op == '*'): print("Output = ",multiply(num1,num2));
